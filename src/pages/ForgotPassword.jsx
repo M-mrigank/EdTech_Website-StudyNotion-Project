b896f88ac7df5e64304814f50806aba4069e7bad
M-mrigank <mrigank.mgr@gmail.com>
2024-09-28 12:17:25 +0530

    update

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -22,45 +22,50 @@ const ForgotPassword = () => {
     <div className='text-white flex justify-center items-center'>
       {
         loading?(<div>Loading ...</div>):(
-            <div>
-                <h1>
+            <div className='mx-auto w-11/12 md:mx-0 flex justify-center items-center flex-col mt-10 '>
+                <h1 className='text-[1.875rem] font-semibold leading-[2.375rem] text-richblack-5'>
                     {
                         !emailSent?"Reset Your Password":"Check Your Email"
                     }
                 </h1>
-                <p>
+                <p className="mt-4 mb-3 text-[1.125rem] leading-[1.625rem] text-richblack-300">
                     {
                         !emailSent?"Have no fear. We’ll email you instructions to reset your password. If you dont have access to your email we can try account recovery":`We have sent the reset email to ${email}`
                     }
                 </p>
-                <form onSubmit={handleOnSubmit}>
+                <form onSubmit={handleOnSubmit} className="flex w-[60%] flex-col gap-y-4">
                     {
                         !emailSent && (
-                            <label>
-                                <p>Email Address:</p>
+                            <label className="w-full flex space-x-3 justify-center items-center">
+                                <p className="mb-1 text-[1rem] leading-[1.375rem] text-richblack-5">Email Address:</p>
                                 <input 
-                                    className='w-full p-6 bg-richblack-600 text-richblack-100'
                                     required
                                     type='email'
                                     value={email}
                                     name='email'
                                     onChange={(e)=>setEmail(e.target.value)}
                                     placeholder='Enter Your Email Address'
+                                    className='w-[70%] p-6 bg-richblack-600 text-richblack-100 rounded-[0.5rem]'
+                                    style={{
+                                        boxShadow: "inset 0px -3px 0px rgba(255, 255, 255, 0.18)",
+                                    }}
                                 />
                             </label>
                         )
                     }
-                    <button type='submit'>
-                        {
-                            !emailSent?"Reset Password": "Resend Email"
-                        }
-                    </button>
+                    <div className='flex justify-around items center mt-6'>
+                        <button type='submit' className="rounded-[8px] bg-yellow-50 py-[8px] px-[12px] font-semibold text-richblack-900">
+                            {
+                                !emailSent?"Reset Password": "Resend Email"
+                            }
+                        </button>
+                        <div className="rounded-[8px] bg-richblack-50 py-[8px] px-[12px] font-medium text-richblack-600 flex justify-center items-center">
+                            <Link to={"/login"}>
+                                <p className="mb-1 font-semibold text-[1rem] leading-[1.375rem] text-slate-500">Back to Login</p>
+                            </Link>
+                        </div>
+                    </div>
                 </form>
-                <div>
-                    <Link to={"/login"}>
-                        <p>Back to Login</p>
-                    </Link>
-                </div>
             </div>
         )
       }
